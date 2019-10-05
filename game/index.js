2e9e97d576e507c3d4236c055c54fca3b849fa45
Victoria2343 <vikusya234@mail.ru>
2019-10-05 22:01:59 +0300

    add While and If

--- game/index.js
+++ game/index.js
@@ -1,10 +1,34 @@
 let name = prompt("Привет! Как тебя зовут?");
 
-if (name == "") {
+while (name == "" || name == null) {
   name = prompt("Привет! Как тебя зовут?");
 }
 
 alert(
   name +
     ', я загадал число от 1 до 100. Попробуй отгадать его за наименьшее количество попыток. После каждой попытки я скажу "мало", "много" или "угадал".'
+);
+
+let number = 42;
+let guess = prompt("Какое число я загадал?");
+let numberOfGuesses = 0;
+
+while (guess != number) {
+  if (guess > number) {
+    guess = prompt("Много. Попробуй еще раз");
+    numberOfGuesses = numberOfGuesses + 1;
+  }
+
+  if (guess < number) {
+    guess = prompt("Мало. Попробуй еще раз");
+    numberOfGuesses = numberOfGuesses + 1;
+  }
+}
+
+alert(
+  "Ты угадал! Это число " +
+    number +
+    ". Тебе понадобилось " +
+    numberOfGuesses +
+    " попыток."
 );
