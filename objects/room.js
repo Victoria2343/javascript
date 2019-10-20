8145ad56981552e90feba287e8e7dd8918969d87
Victoria2343 <vikusya234@mail.ru>
2019-10-20 14:34:33 +0300

    add object Room

---
+++ objects/room.js
@@ -0,0 +1,27 @@
+let room = {
+  name: "Моя комната"
+};
+
+room.table = { name: "Стол" };
+room.table.color = "brown";
+room.table.pencil = { name: "Карандаш" };
+room.table.pc = { name: "Мой компьютер" };
+room.table.copybook = { name: "Тетрадь" };
+
+room.wardrobe = { name: "Шкаф для одежды" };
+room.wardrobe.color = "brown";
+
+room.sofa = { name: "Диван" };
+room.sofa.color = "beige";
+
+room.tvset = { name: "Телевизор" };
+room.tvset.model = "Samsung";
+
+room.bookcase = { name: "Книжный шкаф" };
+room.bookcase.color = "brown";
+room.bookcase.book = "The Hobbit";
+
+room.flowerpot1 = { name: "Ficus" };
+room.flowerpot2 = { name: "Money Tree" };
+
+alert(room);
